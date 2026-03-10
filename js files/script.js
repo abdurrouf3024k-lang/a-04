bdf92fd33197d55e94d44d630f413335d75a0ac0
Abdur Rauf <abdurrouf3024k@gmail.com>
2026-03-11 01:06:54 +0600

    reject buttons funtionality added

--- js files/script.js
+++ js files/script.js
@@ -50,12 +50,16 @@ function toogleStyle(id){
     allCardSection.classList.remove('hidden');
     filteredSection.classList.add('hidden');
   }
+  else if(id == 'rejected-filter-btn'){
+    allCardSection.classList.add('hidden');
+    filteredSection.classList.remove('hidden');
+  }
    
 }
 
 mainContainer.addEventListener('click', function(event){
 
-  console.log(event.target.classList.contains('interview-btn'))
+  
 
  if (event.target.classList.contains('interview-btn')){
    const parentNode = event.target.parentNode.parentNode;
@@ -77,6 +81,27 @@ mainContainer.addEventListener('click', function(event){
    calculateCount();
   renderInterview()
   
+ }
+ 
+ else if (event.target.classList.contains('rejected-btn')){
+   const parentNode = event.target.parentNode.parentNode;
+  const chakrirName = parentNode.querySelector('.ChakrirName').innerText;
+  const developerType = parentNode.querySelector('.developerType').innerText;
+  const salaryDollar = parentNode.querySelector('.salaryDollar').innerText;
+  const applicableBtn = parentNode.querySelector('.applicableBtn').innerText;
+  const jobDescription = parentNode.querySelector('.jobDescription').innerText;
+  
+
+  const cardInfo = { chakrirName, developerType, salaryDollar, applicableBtn : 'Rejected', jobDescription };
+  const jobExist = rejectList.find(item => item.chakrirName == cardInfo.chakrirName);
+
+  parentNode.querySelector('.applicableBtn').innerText = 'Rejected';
+
+  if(!jobExist){
+    rejectList.push(cardInfo);
+  }
+   calculateCount();
+   renderRejected();
  }
 })
 
@@ -100,6 +125,43 @@ for(let interview of interviewList){
               <p class="salaryDollar text-[#64748B] mt-[20px]">Remote • Full-time • $130,000 - $175,000</p>
             </div>
             <p  class="applicableBtn text-[14px] font-medium p-[12px] bg-[#EEF4FF] w-[200px] rounded-xl text-center mt-[20px]">${interview.applicableBtn}</p>
+            <p class="jobDescription font-[14px] font-regular text-[#323B49] mt-[8px]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <div class="mt-[20px]">
+              <button class="font-[14px] font-semibold text-[#10B981] bg-[#FFFFFF] p-[12px] rounded-xl border border-[#10B981] mr-[8px]" id="interview-btn">INTERVIEW</button>
+              <button class="font-[14px] font-semibold text-[#EF4444] bg-[#FFFFFF] p-[12px] rounded-xl border border-[#EF4444]" id="rejected-btn">REJECTED</button>
+            </div>
+          </div>
+
+          <!-- main part-2 -->
+          <div>
+            <button class="dlt-btn"><i class="fas fa-trash"></i></button>
+          </div>`
+
+  filteredSection.appendChild(div);
+} 
+}
+
+
+
+function renderRejected(){
+filteredSection.innerHTML = '';
+
+
+
+for(let rejected of rejectList){
+  
+  console.log(rejected);
+  
+  let div = document.createElement('div');
+  div.className = 'flex justify-between p-[24px] bg-[#FFFFFF] rounded-xl mt-[24px]'
+  div.innerHTML = ` <!-- main part-1 -->
+          <div>
+            <div>
+              <h1 class="ChakrirName font-semibold text-[18px] text-[#002C5C]">${rejected.chakrirName}</h1>
+              <p class="developerType text-[#64748B]">React Native Developer</p>
+              <p class="salaryDollar text-[#64748B] mt-[20px]">Remote • Full-time • $130,000 - $175,000</p>
+            </div>
+            <p  class="applicableBtn text-[14px] font-medium p-[12px] bg-[#EEF4FF] w-[200px] rounded-xl text-center mt-[20px]">${rejected.applicableBtn}</p>
             <p class="jobDescription font-[14px] font-regular text-[#323B49] mt-[8px]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
             <div class="mt-[20px]">
               <button class="font-[14px] font-semibold text-[#10B981] bg-[#FFFFFF] p-[12px] rounded-xl border border-[#10B981] mr-[8px]" id="interview-btn">INTERVIEW</button>
